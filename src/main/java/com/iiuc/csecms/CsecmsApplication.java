29b80471a1f34adda416ad6deb74de8c4a586f90
fahim15hub <muntasirf15@gmail.com>
2025-12-23 13:39:04 +0600

    Add application entry point and base test

---
+++ src/main/java/com/iiuc/csecms/CsecmsApplication.java
@@ -0,0 +1,13 @@
+package com.iiuc.csecms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsecmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsecmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iiuc/csecms/CsecmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiuc.csecms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsecmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
